3701d510396d72eee532c995fa8754c5d0370ea6
Shiyou_K <404212584@qq.com>
2020-06-06 16:26:39 +1000

    required login

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,11 @@ from app.forms import LoginForm
 from flask_login import current_user, login_user
 from app.models import User
 from flask_login import logout_user
+from flask_login import login_required
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -19,7 +21,10 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template("index.html", title='Home Page', posts=posts)
+
+from flask import request
+from werkzeug.urls import url_parse
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -32,7 +37,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
